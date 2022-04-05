1a0c17bd81bd75e5411c4019d050d40ee6156ab1
reinans <reinandesouza01@gmail.com>
2022-04-05 09:23:46 -0300

    first commit

---
+++ src/test/java/edu/com/pweb/newsblog/NewsblogApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.com.pweb.newsblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
