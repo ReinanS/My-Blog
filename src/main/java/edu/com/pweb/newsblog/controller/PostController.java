d13b197545a27fa470c1af341bafbb289b30ba24
reinans <reinandesouza01@gmail.com>
2022-04-11 15:06:08 -0300

    feat: add ResponseEntity

--- src/main/java/edu/com/pweb/newsblog/controller/PostController.java
+++ src/main/java/edu/com/pweb/newsblog/controller/PostController.java
@@ -2,6 +2,8 @@ package edu.com.pweb.newsblog.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +21,9 @@ public class PostController {
     final PostService postService;
 
     @GetMapping
-    public List<PostOut> listar() {
-        return postService.listar();
-    }
-    
+    public ResponseEntity<List<PostOut>> listar() {
+
+        List<PostOut> posts = postService.listar();
+        return new ResponseEntity<List<PostOut>>(posts, HttpStatus.OK);
+    }    
 }
